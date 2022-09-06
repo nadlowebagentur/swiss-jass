1a50034f3c7c72abeb9ae07bffa18b1ab90da475
Leonid Marchenko <codobred@gmail.com>
2022-09-06 09:58:28 +0300

    install pm2 locally

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-npm install && npm i -g pm2 && npm run prod && pm2 start ./ecosystem.config.js
+npm install && npm run prod && ./node_modules/.bin/pm2 start ./ecosystem.config.js
 
 # pm2 save 
 # pm2 startup
